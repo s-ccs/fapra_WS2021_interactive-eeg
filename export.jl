96c57216c522f516ded72eca3e76e929dddf4d85
Luis Lips <luis@luis-3.fritz.box>
2022-04-12 10:53:38 +0200

    directed exports to separate folder

--- export.jl
+++ export.jl
@@ -2,4 +2,4 @@
 path_to_dir = abspath("./notebooks/")
 @info path_to_dir
 using PlutoSliderServer
-PlutoSliderServer.export_directory(path_to_dir)
+PlutoSliderServer.export_directory(path_to_dir, Export_output_dir="./output/", Export_offer_binder=false)
